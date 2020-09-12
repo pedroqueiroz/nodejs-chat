6c630b489805db0dc6c997c214dce5a4f58a36aa
Pedro Queiroz <pedroqueiroz942@gmail.com>
2020-09-12 17:40:18 -0300

    [Service] Allow users to connect and send message to the server

--- chat-service/src/app.ts
+++ chat-service/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import http from 'http'
+import WebSocket from 'ws'
 
 const app = express()
 
@@ -8,6 +10,18 @@ app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
-app.listen(PORT, () => {
-  console.log(`server started at http://localhost:${PORT}`)
+const server = http.createServer(app)
+
+const webSocketServer = new WebSocket.Server({ server })
+
+webSocketServer.on('connection', (webSocket) => {
+  webSocket.on('message', (message) => {
+    webSocket.send(`You sent: ${message}`)
+  })
+
+  webSocket.send('Connection Stablished!')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
 })
