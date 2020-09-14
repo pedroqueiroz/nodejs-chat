0533fc5a19bb4c883d4d6b904692c8c660615281
Pedro Queiroz <pedroqueiroz942@gmail.com>
2020-09-13 21:51:00 -0300

    [Client] Add route to ChatPage

--- chat-client/src/App/App.tsx
+++ chat-client/src/App/App.tsx
@@ -1,76 +1,18 @@
-import React, { useState, useEffect, FunctionComponent } from 'react'
-import {
-  Box,
-  CircularProgress,
-  Container,
-  Paper,
-  List,
-  ListItem,
-  ListItemText,
-} from '@material-ui/core'
+import React, { FunctionComponent } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import UserMessageInput from './components/UserMessageInput/UserMessageInput'
-import { ChatMessage } from 'types/chat'
+import ChatPage from 'pages/ChatPage/ChatPage'
 
 import './App.css'
 
-const SERVER_ADDRESS = 'ws://localhost:8080'
-const CURRENT_USER = 'Pedro'
-
-const webSocket = new WebSocket(SERVER_ADDRESS)
-
-const App: FunctionComponent = () => {
-  const [isReady, setIsReady] = useState(false)
-  const [messages, setMessages] = useState<Array<ChatMessage>>([])
-  const [userMessage, setUserMessage] = useState<string>('')
-
-  useEffect(() => {
-    webSocket.onopen = () => setIsReady(true)
-
-    webSocket.onmessage = (event) => {
-      setMessages([...messages, JSON.parse(event.data)])
-    }
-  })
-
-  if (!isReady) {
-    return (
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        height="100vh"
-      >
-        <CircularProgress />
-      </Box>
-    )
-  }
-
-  return (
-    <Container maxWidth="lg">
-      <Paper>
-        <List>
-          {messages.map(({ userName, message }: ChatMessage, index) => (
-            <ListItem key={index}>
-              <ListItemText primary={userName} secondary={message} />
-            </ListItem>
-          ))}
-        </List>
-        <UserMessageInput
-          userMessage={userMessage}
-          setUserMessage={setUserMessage}
-          handleSubmit={() => {
-            webSocket.send(
-              JSON.stringify({
-                userName: CURRENT_USER,
-                message: userMessage,
-              })
-            )
-            setUserMessage('')
-          }}
-        />
-      </Paper>
-    </Container>
-  )
-}
+const App: FunctionComponent = () => (
+  <Router>
+    <Switch>
+      <Route path="/chat">
+        <ChatPage />
+      </Route>
+    </Switch>
+  </Router>
+)
 
 export default App
---
+++ chat-client/src/pages/ChatPage/ChatPage.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, FunctionComponent } from 'react'
+import {
+  Box,
+  CircularProgress,
+  Container,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core'
+
+import UserMessageInput from './components/UserMessageInput/UserMessageInput'
+import { ChatMessage } from 'types'
+
+const SERVER_ADDRESS = 'ws://localhost:8080'
+const CURRENT_USER = 'Pedro'
+
+const webSocket = new WebSocket(SERVER_ADDRESS)
+
+const ChatPage: FunctionComponent = () => {
+  const [isReady, setIsReady] = useState(false)
+  const [messages, setMessages] = useState<Array<ChatMessage>>([])
+  const [userMessage, setUserMessage] = useState<string>('')
+
+  useEffect(() => {
+    webSocket.onopen = () => setIsReady(true)
+
+    webSocket.onmessage = (event) => {
+      setMessages([...messages, JSON.parse(event.data)])
+    }
+  })
+
+  if (!isReady) {
+    return (
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+      >
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  return (
+    <Container maxWidth="lg">
+      <Paper>
+        <List>
+          {messages.map(({ userName, message }: ChatMessage, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={userName} secondary={message} />
+            </ListItem>
+          ))}
+        </List>
+        <UserMessageInput
+          userMessage={userMessage}
+          setUserMessage={setUserMessage}
+          handleSubmit={() => {
+            webSocket.send(
+              JSON.stringify({
+                userName: CURRENT_USER,
+                message: userMessage,
+              })
+            )
+            setUserMessage('')
+          }}
+        />
+      </Paper>
+    </Container>
+  )
+}
+
+export default ChatPage
