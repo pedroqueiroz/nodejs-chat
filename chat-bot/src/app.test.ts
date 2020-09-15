04519e0f6d83ab434c9f6a961818575c91f3dfa8
Pedro Queiroz <pedroqueiroz942@gmail.com>
2020-09-14 23:06:44 -0300

    [Bot] Add more tests

--- chat-bot/src/app.test.ts
+++ chat-bot/src/app.test.ts
@@ -1,26 +1,40 @@
 import request from 'supertest'
 
-import CommandService from './services/CommandService'
+import * as CommandService from './services/CommandService'
 import { publishToQueue } from './messageQueue'
 
 import app from './app'
 
-jest.mock('./services/CommandService')
 jest.mock('./messageQueue')
 
 const asMock = (obj: any): jest.Mock => obj as jest.Mock
 
-describe('POST /command', () => {
-  beforeEach(() => {
-    asMock(CommandService.stock).mockReturnValue(
-      'APPL.US quote is $112 per share.'
-    )
-    asMock(publishToQueue).mockReturnValue(null)
-  })
-  it('responds with 200', function (done) {
-    request(app)
-      .post('/command')
-      .send({ command: 'stock', args: { stockCode: 'aapl.us' } })
-      .expect(200, done)
+describe('App', () => {
+  describe('POST /command', () => {
+    beforeEach(() => {
+      asMock(publishToQueue).mockReturnValue(null)
+    })
+    it('should respond with 200 when the command is well formed', function (done) {
+      jest.spyOn(CommandService, 'getQuotePerShare').mockResolvedValue({
+        title: 'AAPL.US',
+        quote: '112'
+      })
+
+      request(app)
+        .post('/command')
+        .send({ command: 'stock', args: { stockCode: 'aapl.us' } })
+        .expect(200, done)
+    })
+
+    it('should respond with 400 when the command does not exist', function (done) {
+      request(app).post('/command').send({ command: 'play' }).expect(400, done)
+    })
+
+    it('should respond with 400 when the command does contain the expected arguments', function (done) {
+      request(app)
+        .post('/command')
+        .send({ command: 'stock', args: undefined })
+        .expect(400, done)
+    })
   })
 })
